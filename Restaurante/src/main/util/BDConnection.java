e155083db80a927ca64dd8f8c51fa8955776425f
MatiasCarmen <u22209935@utp.edu.pe>
2025-06-19 21:45:06 -0500

    Add JPA and secure DB config

--- Restaurante/src/main/util/BDConnection.java
+++ Restaurante/src/main/util/BDConnection.java
@@ -1,35 +1,17 @@
 package main.util;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class BDConnection {
-    private static Properties loadProperties() throws SQLException {
-        Properties props = new Properties();
-        try (FileInputStream input = new FileInputStream("src/main/resources/config.properties")) {
-            props.load(input);
-
-            // Verificar que todas las propiedades necesarias existen
-            if (!props.containsKey("db.url") ||
-                    !props.containsKey("db.user") ||
-                    !props.containsKey("db.password")) {
-                throw new SQLException("Faltan propiedades de configuración necesarias");
-            }
-
-            return props;
-        } catch (IOException e) {
-            throw new SQLException("No se pudo cargar el archivo de configuración", e);
-        }
-    }
-
     public static Connection getConnection() throws SQLException {
-        Properties props = loadProperties();
-        String url = props.getProperty("db.url");
-        String user = props.getProperty("db.user");
-        String password = props.getProperty("db.password");
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        if (url == null || user == null || password == null) {
+            throw new SQLException("Variables de entorno de base de datos no definidas");
+        }
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
